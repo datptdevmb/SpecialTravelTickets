615c4f23deb519f744fa375f4c74d5daef7257e1
datpt <datptdevmb@gmail.com>
2024-06-17 00:47:58 +0700

    update LoginSreen

--- src/screen/LoginScreen.js
+++ src/screen/LoginScreen.js
@@ -5,10 +5,11 @@ import { SafeAreaView, View } from "react-native";
 
 function LoginScreen (){
 
-
     return(
         <SafeAreaView>
-            
+
         </SafeAreaView>
     )
 }
+
+return LoginScreen
