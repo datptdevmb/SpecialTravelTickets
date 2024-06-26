7e4a34f8fca0d1ed535071446ffcbfca83a7c157
minhnhut2306 <129873898+minhnhut2306@users.noreply.github.com>
2024-06-26 20:48:06 +0700

    suabutton

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,30 +1,34 @@
 import {TouchableOpacity, Image} from 'react-native';
 import stylescontainer from '../styles/screens/Containerloginre_style';
 import Icons from '../constants/Icons';
-import { useState } from 'react';
+import {useState} from 'react';
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 
-const BackButton = ({buttonStyle = {}}) => {
+// const BackButton = ({buttonStyle = {}}) => {
 
-    return (
-        <TouchableOpacity style={{...styles.button,...buttonStyle}}>
-            <Svg width="19" height="19" viewBox="0 0 19 19" fill="none"></Svg>
-        </TouchableOpacity>
-    );
-=======
-function BackButton({ onPress }) {
+//     return (
+//         <TouchableOpacity style={{...styles.button,...buttonStyle}}>
+//             <Svg width="19" height="19" viewBox="0 0 19 19" fill="none"></Svg>
+//         </TouchableOpacity>
+//     );
+//   }
+// =======
+function BackButton({onPress}) {
   const [pressnd, setpressnd] = useState(false);
   const handlePress = () => {
     setpressnd(true);
     onPress();
   };
   return (
-    <TouchableOpacity style={stylescontainer.containerLeftArrow} onPress={handlePress}>
+    <TouchableOpacity
+      style={stylescontainer.containerLeftArrow}
+      onPress={handlePress}>
       <Image source={Icons.leftarrow} style={stylescontainer.iconLeftArrow} />
     </TouchableOpacity>
   );
->>>>>>> f4652ae458de1e40e41d3c287d5e72c50dd07686
+  // >>>>>>> f4652ae458de1e40e41d3c287d5e72c50dd07686
 }
 
 export default BackButton;
+n
