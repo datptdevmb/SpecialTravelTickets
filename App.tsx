ea4ae874f25d622113c47c0420e9a333bdce4c1b
minhnhut2306 <129873898+minhnhut2306@users.noreply.github.com>
2024-06-28 15:08:40 +0700

    them file import

--- App.tsx
+++ App.tsx
@@ -6,6 +6,6 @@ import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 
 function App(): React.JSX.Element {
-  return <ForgotScreen />;
+  return <LoginScreen />;
 }
 export default App;
