6bef9d0d7b02345c753b6feb63216559ab3d03a6
minhnhut2306 <129873898+minhnhut2306@users.noreply.github.com>
2024-07-02 13:32:18 +0700

    sua app

--- App.tsx
+++ App.tsx
@@ -11,11 +11,11 @@ const Stack = createStackNavigator();
 function App(): React.JSX.Element {
   return(
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='LoginScreen'>
-        <Stack.Screen name="LoginScreen" component={LoginScreen}  options={{ headerShown: false}}/>
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{ headerTitle: 'Register' }}/>
-        <Stack.Screen name="ForgotScreen" component={ForgotScreen} options={{ headerTitle: 'Forgot Password' }}/>
-        <Stack.Screen name="ResetPassWord" component={ResetPassWord} options={{ headerTitle: 'Reset Password' }}/>
+      <Stack.Navigator initialRouteName='ResetPassWord' screenOptions={{headerShown: false}}>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <Stack.Screen name="ForgotScreen" component={ForgotScreen}/>
+        <Stack.Screen name="ResetPassWord" component={ResetPassWord} />
       </Stack.Navigator>
     </NavigationContainer>
   );
