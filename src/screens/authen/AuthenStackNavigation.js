2b217b0c36d6be576b69f300939403d915b6c5bc
minhnhut2306 <129873898+minhnhut2306@users.noreply.github.com>
2024-07-18 23:27:51 +0700

    bo du thua

--- src/screens/authen/AuthenStackNavigation.js
+++ src/screens/authen/AuthenStackNavigation.js
@@ -1,14 +1,6 @@
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import LoginScreen from './LoginScreen';
-import RegisterScreen from './RegisterScreen';
-import ForgotScreen from './ForgotScreen';
-import ResetPassWord from './ResetPassWordScreen';
-import ChangePasswordSuccess from './ChangePasswordSuccess';
-import {View, Text} from 'react-native';
-import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './LoginScreen';
 import RegisterScreen from './RegisterScreen';
 import ForgotScreen from './ForgotScreen';
@@ -19,27 +11,12 @@ const Stack = createNativeStackNavigator();
 
 const AuthenStackNavigation = () => {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen
-        name="ChangePasswordSuccess"
-        component={ChangePasswordSuccess}
-      />
+      <Stack.Screen name="ChangePasswordSuccess" component={ChangePasswordSuccess} />
       <Stack.Screen name="ForgotScreen" component={ForgotScreen} />
       <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
       <Stack.Screen name="ResetPassWord" component={ResetPassWord} />
-      return (
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="ResetPassWord" component={ResetPassWord} />
-        <Stack.Screen name="ForgotScreen" component={ForgotScreen} />
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen
-          name="ChangePasswordSuccess"
-          component={ChangePasswordSuccess}
-        />
-      </Stack.Navigator>
-      );
     </Stack.Navigator>
   );
 };
